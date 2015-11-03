d356e28de784402cfd2255b6d16a169578e4bcd7
phax <philip@helger.com>
2015-11-03 21:42:43 +0100

    Version update

--- ph-db-jpa/src/main/java/com/helger/db/jpa/config/ThirdPartyModuleProvider_ph_db_jpa.java
+++ ph-db-jpa/src/main/java/com/helger/db/jpa/config/ThirdPartyModuleProvider_ph_db_jpa.java
@@ -45,14 +45,14 @@ public final class ThirdPartyModuleProvider_ph_db_jpa implements IThirdPartyModu
   public static final IThirdPartyModule H2 = new ThirdPartyModule ("H2 Database Engine",
                                                                    "Eclipse Foundation",
                                                                    ELicense.EPL10,
-                                                                   new Version (1, 4, 188),
+                                                                   new Version (1, 4, 190),
                                                                    "http://www.h2database.com/",
                                                                    true);
   /** MySQL connector */
   public static final IThirdPartyModule MYSQL = new ThirdPartyModule ("MySQL Connector/J",
                                                                       "Oracle",
                                                                       ELicense.GPL20,
-                                                                      new Version (5, 1, 36),
+                                                                      new Version (5, 1, 37),
                                                                       "http://www.mysql.com/",
                                                                       true);
 
