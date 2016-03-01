c83a0390c2a0c1dcd9491cc7698a75ea839df99d
Philip Helger <philip@helger.com>
2016-03-01 10:22:03 +0100

    Using new collections API

--- ph-db-api/src/test/java/com/helger/db/api/mysql/MySQLHelperTest.java
+++ ph-db-api/src/test/java/com/helger/db/api/mysql/MySQLHelperTest.java
@@ -2,10 +2,10 @@ package com.helger.db.api.mysql;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.junit.Test;
+
+import com.helger.commons.collection.ext.CommonsLinkedHashMap;
+import com.helger.commons.collection.ext.ICommonsOrderedMap;
 
 /**
  * Test class for class {@link MySQLHelper}.
@@ -18,7 +18,7 @@ public final class MySQLHelperTest
   public void testBasic ()
   {
     assertEquals ("jdbc:mysql:a", MySQLHelper.buildJDBCString ("jdbc:mysql:a", null));
-    final Map <EMySQLConnectionProperty, String> aMap = new LinkedHashMap <> ();
+    final ICommonsOrderedMap <EMySQLConnectionProperty, String> aMap = new CommonsLinkedHashMap <> ();
     assertEquals ("jdbc:mysql:a", MySQLHelper.buildJDBCString ("jdbc:mysql:a", aMap));
     aMap.put (EMySQLConnectionProperty.autoDeserialize, "true");
     assertEquals ("jdbc:mysql:a?autoDeserialize=true", MySQLHelper.buildJDBCString ("jdbc:mysql:a", aMap));
