a2d81d64e681d2c27b195ee923e9d647678698d7
Philip Helger <philip@helger.com>
2017-03-23 11:00:43 +0100

    Version update

--- ph-db-jdbc/src/main/java/com/helger/db/jdbc/config/ThirdPartyModuleProvider_ph_db_jdbc.java
+++ ph-db-jdbc/src/main/java/com/helger/db/jdbc/config/ThirdPartyModuleProvider_ph_db_jdbc.java
@@ -57,7 +57,7 @@ public final class ThirdPartyModuleProvider_ph_db_jdbc implements IThirdPartyMod
   public static final IThirdPartyModule MYSQL = new ThirdPartyModule ("MySQL Connector/J",
                                                                       "Oracle",
                                                                       ELicense.GPL20,
-                                                                      new Version (6, 0, 5),
+                                                                      new Version (6, 0, 6),
                                                                       "http://www.mysql.com/",
                                                                       true);
 
--- ph-db-jpa/src/main/java/com/helger/db/jpa/config/ThirdPartyModuleProvider_ph_db_jpa.java
+++ ph-db-jpa/src/main/java/com/helger/db/jpa/config/ThirdPartyModuleProvider_ph_db_jpa.java
@@ -52,7 +52,7 @@ public final class ThirdPartyModuleProvider_ph_db_jpa implements IThirdPartyModu
   public static final IThirdPartyModule MYSQL = new ThirdPartyModule ("MySQL Connector/J",
                                                                       "Oracle",
                                                                       ELicense.GPL20,
-                                                                      new Version (6, 0, 5),
+                                                                      new Version (6, 0, 6),
                                                                       "http://www.mysql.com/",
                                                                       true);
 
