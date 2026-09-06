be2cb30cc5895da28e00f906561858e35049b2d1
Philip Helger <philip@helger.com>
2026-09-06 09:33:11 +0200

    Using telemetry 1.0.2

--- ph-db-flyway/src/test/java/com/helger/db/flyway/FlywayMigrationRunnerTelemetryTest.java
+++ ph-db-flyway/src/test/java/com/helger/db/flyway/FlywayMigrationRunnerTelemetryTest.java
@@ -21,37 +21,21 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-import java.time.Duration;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.function.LongSupplier;
-
 import org.flywaydb.core.api.callback.Callback;
 import org.flywaydb.core.api.migration.JavaMigration;
 import org.jspecify.annotations.NonNull;
-import org.jspecify.annotations.Nullable;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.helger.collection.commons.CommonsArrayList;
-import com.helger.collection.commons.CommonsLinkedHashMap;
-import com.helger.collection.commons.ICommonsList;
-import com.helger.collection.commons.ICommonsOrderedMap;
 import com.helger.db.api.EDatabaseSystemType;
 import com.helger.db.api.config.IJdbcConfiguration;
 import com.helger.db.api.config.JdbcConfiguration;
 import com.helger.db.api.telemetry.CDBTelemetry;
 import com.helger.telemetry.ETelemetrySpanKind;
-import com.helger.telemetry.ITelemetryCounter;
-import com.helger.telemetry.ITelemetryGauge;
-import com.helger.telemetry.ITelemetryHistogram;
-import com.helger.telemetry.ITelemetryMeterSPI;
-import com.helger.telemetry.ITelemetrySpan;
-import com.helger.telemetry.ITelemetryTracerSPI;
-import com.helger.telemetry.ITelemetryUpDownCounter;
-import com.helger.telemetry.Telemetry;
-import com.helger.telemetry.TelemetryAttributes;
-import com.helger.telemetry.TelemetryMetrics;
+import com.helger.telemetry.mock.CapturingTelemetry;
+import com.helger.telemetry.mock.CapturingTelemetry.CapturedMeasurement;
+import com.helger.telemetry.mock.CapturingTelemetry.CapturedSpan;
 
 /**
  * Test class for the ph-telemetry integration of {@link FlywayMigrationRunner}. It runs a real
@@ -64,187 +48,20 @@ public final class FlywayMigrationRunnerTelemetryTest
   private static final String JDBC_URL = "jdbc:h2:mem:phdbflywaytelemetry;DB_CLOSE_DELAY=-1";
   private static final String LOCATION = "db/migrate-test";
 
-  // TODO ph-telemetry 1.0.2: replace the local test doubles below with com.helger.telemetry.mock.CapturingTelemetry
-  /** A span that only records what was set on it. */
-  private static final class CapturingSpan implements ITelemetrySpan
-  {
-    private final String m_sName;
-    private final ETelemetrySpanKind m_eKind;
-    private final ICommonsOrderedMap <String, Object> m_aAttrs = new CommonsLinkedHashMap <> ();
-    private Throwable m_aException;
-    private boolean m_bClosed;
-
-    CapturingSpan (@NonNull final String sName, @NonNull final ETelemetrySpanKind eKind)
-    {
-      m_sName = sName;
-      m_eKind = eKind;
-    }
-
-    @NonNull
-    public ITelemetrySpan setAttribute (@NonNull final String sKey, @Nullable final String sValue)
-    {
-      if (sValue != null)
-        m_aAttrs.put (sKey, sValue);
-      return this;
-    }
-
-    @NonNull
-    public ITelemetrySpan setAttribute (@NonNull final String sKey, final boolean bValue)
-    {
-      m_aAttrs.put (sKey, Boolean.valueOf (bValue));
-      return this;
-    }
-
-    @NonNull
-    public ITelemetrySpan setAttribute (@NonNull final String sKey, final long nValue)
-    {
-      m_aAttrs.put (sKey, Long.valueOf (nValue));
-      return this;
-    }
-
-    @NonNull
-    public ITelemetrySpan setAttribute (@NonNull final String sKey, final double dValue)
-    {
-      m_aAttrs.put (sKey, Double.valueOf (dValue));
-      return this;
-    }
-
-    @NonNull
-    public ITelemetrySpan recordException (@NonNull final Throwable aException)
-    {
-      m_aException = aException;
-      return this;
-    }
-
-    @NonNull
-    public ITelemetrySpan addEvent (@NonNull final String sName, @NonNull final TelemetryAttributes aAttributes)
-    {
-      return this;
-    }
-
-    @NonNull
-    public ITelemetrySpan setStatusOk ()
-    {
-      return this;
-    }
-
-    @NonNull
-    public ITelemetrySpan setStatusError (@Nullable final String sMessage)
-    {
-      return this;
-    }
-
-    public void close ()
-    {
-      m_bClosed = true;
-    }
-  }
-
-  private static final class CapturingTracer implements ITelemetryTracerSPI
-  {
-    private final ICommonsList <CapturingSpan> m_aSpans = new CommonsArrayList <> (new CopyOnWriteArrayList <> ());
-
-    @NonNull
-    public ITelemetrySpan startSpan (@NonNull final String sName, @NonNull final ETelemetrySpanKind eKind)
-    {
-      final CapturingSpan ret = new CapturingSpan (sName, eKind);
-      m_aSpans.add (ret);
-      return ret;
-    }
-  }
-
-  private record Measurement (String sInstrument, double dValue, ICommonsOrderedMap <String, Object> aAttrs)
-  {}
-
-  private static final class CapturingMeter implements ITelemetryMeterSPI
-  {
-    private final ICommonsList <Measurement> m_aMeasurements = new CommonsArrayList <> (new CopyOnWriteArrayList <> ());
-
-    @NonNull
-    private static ICommonsOrderedMap <String, Object> _toMap (@NonNull final TelemetryAttributes aAttrs)
-    {
-      final ICommonsOrderedMap <String, Object> ret = new CommonsLinkedHashMap <> ();
-      aAttrs.forEach (new TelemetryAttributes.IVisitor ()
-      {
-        public void onString (@NonNull final String sKey, @NonNull final String sValue)
-        {
-          ret.put (sKey, sValue);
-        }
-
-        public void onLong (@NonNull final String sKey, final long nValue)
-        {
-          ret.put (sKey, Long.valueOf (nValue));
-        }
-
-        public void onDouble (@NonNull final String sKey, final double dValue)
-        {
-          ret.put (sKey, Double.valueOf (dValue));
-        }
-
-        public void onBoolean (@NonNull final String sKey, final boolean bValue)
-        {
-          ret.put (sKey, Boolean.valueOf (bValue));
-        }
-      });
-      return ret;
-    }
-
-    private void _record (@NonNull final String sName, final double dValue, @NonNull final TelemetryAttributes aAttrs)
-    {
-      m_aMeasurements.add (new Measurement (sName, dValue, _toMap (aAttrs)));
-    }
-
-    @NonNull
-    public ITelemetryCounter createCounter (@NonNull final String sName,
-                                            @Nullable final String sDescription,
-                                            @Nullable final String sUnit)
-    {
-      return (nValue, aAttrs) -> _record (sName, nValue, aAttrs);
-    }
-
-    @NonNull
-    public ITelemetryUpDownCounter createUpDownCounter (@NonNull final String sName,
-                                                        @Nullable final String sDescription,
-                                                        @Nullable final String sUnit)
-    {
-      return (nValue, aAttrs) -> _record (sName, nValue, aAttrs);
-    }
-
-    @NonNull
-    public ITelemetryHistogram createHistogram (@NonNull final String sName,
-                                                @Nullable final String sDescription,
-                                                @Nullable final String sUnit)
-    {
-      return (dValue, aAttrs) -> _record (sName, dValue, aAttrs);
-    }
-
-    @NonNull
-    public ITelemetryGauge createGauge (@NonNull final String sName,
-                                        @Nullable final String sDescription,
-                                        @Nullable final String sUnit,
-                                        @NonNull final LongSupplier aSupplier)
-    {
-      return () -> {};
-    }
-  }
-
-  private static final CapturingTracer TRACER = new CapturingTracer ();
-  private static final CapturingMeter METER = new CapturingMeter ();
+  private static final CapturingTelemetry CT = new CapturingTelemetry ();
 
   @BeforeClass
   public static void installTelemetry ()
   {
     // Must happen before FlywayMetrics is class-loaded, because the instruments are resolved once
     // in its static initializer
-    Telemetry.install (TRACER);
-    TelemetryMetrics.install (METER);
+    CT.install ();
   }
 
   @AfterClass
   public static void uninstallTelemetry ()
   {
-    Telemetry.install (null);
-    TelemetryMetrics.install (null);
+    CapturingTelemetry.uninstall ();
   }
 
   @NonNull
@@ -283,30 +100,28 @@ public final class FlywayMigrationRunnerTelemetryTest
                                      new JavaMigration [0],
                                      new Callback [0]);
 
-    assertEquals (1, TRACER.m_aSpans.size ());
-    final CapturingSpan aSpan = TRACER.m_aSpans.getFirstOrNull ();
+    assertEquals (1, CT.getSpanCount ());
+    final CapturedSpan aSpan = CT.getSpans ().getFirstOrNull ();
     assertNotNull (aSpan);
-    assertEquals (CDBTelemetry.SPAN_FLYWAY_MIGRATE, aSpan.m_sName);
-    assertEquals (ETelemetrySpanKind.CLIENT, aSpan.m_eKind);
-    assertEquals (CDBTelemetry.COMPONENT_FLYWAY, aSpan.m_aAttrs.get (CDBTelemetry.ATTR_COMPONENT));
-    assertEquals (EDatabaseSystemType.H2.getID (), aSpan.m_aAttrs.get (CDBTelemetry.ATTR_DB_SYSTEM_NAME));
-    assertEquals (LOCATION, aSpan.m_aAttrs.get (CDBTelemetry.ATTR_FLYWAY_LOCATION));
-    assertEquals (Boolean.FALSE, aSpan.m_aAttrs.get (CDBTelemetry.ATTR_FLYWAY_REPAIR_MODE));
-    assertEquals (Long.valueOf (1), aSpan.m_aAttrs.get (CDBTelemetry.ATTR_FLYWAY_MIGRATIONS_EXECUTED));
-    assertEquals (Boolean.TRUE, aSpan.m_aAttrs.get (CDBTelemetry.ATTR_SUCCESS));
-    assertNull (aSpan.m_aException);
-    assertTrue (aSpan.m_bClosed);
+    assertEquals (CDBTelemetry.SPAN_FLYWAY_MIGRATE, aSpan.getName ());
+    assertEquals (ETelemetrySpanKind.CLIENT, aSpan.getKind ());
+    assertEquals (CDBTelemetry.COMPONENT_FLYWAY, aSpan.getAttribute (CDBTelemetry.ATTR_COMPONENT));
+    assertEquals (EDatabaseSystemType.H2.getID (), aSpan.getAttribute (CDBTelemetry.ATTR_DB_SYSTEM_NAME));
+    assertEquals (LOCATION, aSpan.getAttribute (CDBTelemetry.ATTR_FLYWAY_LOCATION));
+    assertEquals (Boolean.FALSE, aSpan.getAttribute (CDBTelemetry.ATTR_FLYWAY_REPAIR_MODE));
+    assertEquals (Long.valueOf (1), aSpan.getAttribute (CDBTelemetry.ATTR_FLYWAY_MIGRATIONS_EXECUTED));
+    assertEquals (Boolean.TRUE, aSpan.getAttribute (CDBTelemetry.ATTR_SUCCESS));
+    assertNull (aSpan.getRecordedException ());
+    assertTrue (aSpan.isClosed ());
 
-    final Measurement aMigrations = METER.m_aMeasurements.findFirst (x -> x.sInstrument ()
-                                                                           .equals (CDBTelemetry.METRIC_FLYWAY_MIGRATIONS));
+    final CapturedMeasurement aMigrations = CT.getFirstMeasurement (CDBTelemetry.METRIC_FLYWAY_MIGRATIONS);
     assertNotNull (aMigrations);
-    assertEquals (1, (long) aMigrations.dValue ());
-    assertEquals (EDatabaseSystemType.H2.getID (), aMigrations.aAttrs ().get (CDBTelemetry.ATTR_DB_SYSTEM_NAME));
+    assertEquals (1, (long) aMigrations.getValue ());
+    assertEquals (EDatabaseSystemType.H2.getID (), aMigrations.getAttribute (CDBTelemetry.ATTR_DB_SYSTEM_NAME));
 
-    final Measurement aDuration = METER.m_aMeasurements.findFirst (x -> x.sInstrument ()
-                                                                         .equals (CDBTelemetry.METRIC_FLYWAY_MIGRATE_DURATION));
+    final CapturedMeasurement aDuration = CT.getFirstMeasurement (CDBTelemetry.METRIC_FLYWAY_MIGRATE_DURATION);
     assertNotNull (aDuration);
-    assertEquals (Boolean.TRUE, aDuration.aAttrs ().get (CDBTelemetry.ATTR_SUCCESS));
-    assertNull (aDuration.aAttrs ().get (CDBTelemetry.ATTR_ERROR_TYPE));
+    assertEquals (Boolean.TRUE, aDuration.getAttribute (CDBTelemetry.ATTR_SUCCESS));
+    assertNull (aDuration.getAttribute (CDBTelemetry.ATTR_ERROR_TYPE));
   }
 }
